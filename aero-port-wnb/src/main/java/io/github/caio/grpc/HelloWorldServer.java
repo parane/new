2b8bccb8627039c9b202c71caab688658a45e7fe
Paraneetharan Saravanaperumal <psaravanaperumal@isa.ae>
2016-05-18 10:34:10 +0530

    aero-port-wnb

---
+++ aero-port-wnb/src/main/java/io/github/caio/grpc/HelloWorldServer.java
@@ -0,0 +1,17 @@
+package io.github.caio.grpc;
+
+
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+public class HelloWorldServer {
+
+
+    public static void main(String[] args) throws Exception {
+       System.out.println("hello");
+
+    }
+
+
+}
